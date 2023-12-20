4afd56d58f3d60b25529bb970b4e6777724397d4
juju2181 <anishshilpakar8@gmail.com>
2023-12-20 12:06:33 +0545

    Added hello world flask app

---
+++ hello.py
@@ -0,0 +1,11 @@
+# hello.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World 🌍!'
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
